474071e3af8ee2bde67bae8bf6056da73109ec35
aleafs <zhangxc83@gmail.com>
2012-11-06 09:59:47 +0800

    properties parse works fine

--- test/build.test.js
+++ test/build.test.js
@@ -6,10 +6,52 @@ var Build   = require(__dirname + '/../');
 
 describe('build library', function() {
 
-  /* {{{ should_build_init_works_fine() */
-  xit('should_build_init_works_fine', function() {
-    var _me = Build.init(undefined, __dirname);
+  /* {{{ should_build_create_without_file_works_fine() */
+  it('should_build_create_without_file_works_fine', function () {
+    var _me = Build.create(null, null, {'foo' : 231, 'bar' : 'xxyz', 'zero' : 0});
+    _me.get('foo').should.eql(231);
+    _me.get('bar').should.eql('xxyz');
+    _me.get('abc', 'zero').should.eql('zero');
+    _me.get('zero').should.eql(0);
 
+    var a = _me.properties();
+    a.should.eql({
+      'foo' : 231,
+      'bar' : 'xxyz',
+      'zero': 0,
+    });
+
+    // XXX: test for clone
+    a.bar = 'abc';
+    a.should.eql({
+      'foo' : 231,
+      'bar' : 'abc',
+      'zero': 0,
+    });
+    _me.properties().should.eql({
+      'foo' : 231,
+      'bar' : 'xxyz',
+      'zero': 0,
+    });
+  });
+  /* }}} */
+
+  /* {{{ should_build_init_works_fine() */
+  it('should_build_init_works_fine', function() {
+
+    var _me = Build.create('i_am_not_exists');
+    _me.properties().should.eql({});
+
+    var _me = Build.create(__dirname + '/tpl/test1.properties', __dirname);
+    _me.properties().should.eql({
+      'test.c1' : '123dsf=4 5有效',
+      'test.c2' : '"replace last data"',
+      'test.c3' : '1345281197672',
+      'test.c4' : '##i.will.not.be.found##',
+      'test.c5' : '1345281197672'
+    });
+
+    return;
     var the = (new Date()).getTime();
 
     _me.makedir(__dirname + '/etc/build').makedir('etc/build');
